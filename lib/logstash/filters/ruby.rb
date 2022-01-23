0a07cd080d0cce00f91848478dbb0359b1cf2f37
Kaise Cheng <kaise.cheng@elastic.co>
2022-01-23 21:17:48 +0000

    handle SyntaxError exceptions, so the rest of the pipeline can shutdown gracefully Fixed: #32

--- lib/logstash/filters/ruby.rb
+++ lib/logstash/filters/ruby.rb
@@ -66,6 +66,11 @@ class LogStash::Filters::Ruby < LogStash::Filters::Base
     else
       @logger.fatal("You must either use an inline script with the \"code\" option or a script file using \"path\".")
     end
+  rescue SyntaxError => e
+    @logger.error("Syntax Error in register #{e.message}",
+                  :class     => e.class.name,
+                  :backtrace => e.backtrace)
+    fail("Ruby script syntax error")
   end
 
   def self.check_result_events!(results)
